d3697dc4ccb0894e2b6532b4ddc38bdcebf2318e
Dylan Gomes Felix <06834845160@ifb.local>
2025-11-06 15:46:52 -0300

    start commit

---
+++ Exercicios do aluno/questao6.java
@@ -0,0 +1,5 @@
+//Ler o número de alunos existentes em uma turma e, após isto, ler as notas destes alunos, calcular e escrever a média aritmética dessas notas lidas. 
+
+public class questao6 {
+    
+}
