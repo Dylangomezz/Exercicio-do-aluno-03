ea22f0402f3e13c22e09383b687d0eced84b7afe
Dylan Gomes Felix <06834845160@ifb.local>
2025-11-06 15:09:09 -0300

    start commit

---
+++ Exercicios do aluno/questao1.java
@@ -0,0 +1,19 @@
+//Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive). Considere que o N será sempre maior que ZERO.
+
+import java.util.Scanner;   
+
+    public class questao1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Digite um valor inteiro maior que zero: ");
+            int N = scanner.nextInt();
+            
+            System.out.println("Valores inteiros entre 1 e " + N + ":");
+            for (int i = 1; i <= N; i++) {
+                System.out.println(i);
+            }
+            
+            scanner.close();
+        }
+    }
